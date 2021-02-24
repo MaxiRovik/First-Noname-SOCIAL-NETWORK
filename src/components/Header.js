ab94697fa28f45a554eee8eb8b862c068a908342
Max <maxirovik@gmail.com>
2021-02-24 16:56:10 +0200

    first structure

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className = 'header'>
+      <img src = 'http://anatoliy-peresada.narod.ru/energoda/gERB.JPG'/>
+    </header>
+    
+}
+
+export default Header;
